9b0cea8d424b2a639c181939c76bcaac3900a796
starminalush <alica154323@gmail.com>
2023-07-04 01:49:06 +0500

    fix some files

--- backend/crud/event.py
+++ backend/crud/event.py
@@ -1,12 +1,14 @@
 from dataclasses import asdict
 
-from core.db.models import Event
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.db.models import Event
 from data_classes.emotion_recognition import EmotionRecognitionHistoryEvent
+
 
 async def add_recognition_event_to_db(
     db: AsyncSession,
-    event_list:list[EmotionRecognitionHistoryEvent],
+    event_list: list[EmotionRecognitionHistoryEvent],
 ):
     events_db = [Event(**asdict(event)) for event in event_list]
     db.add_all(events_db)
--- backend/schemas/emotion_recognition.py
+++ backend/schemas/emotion_recognition.py
@@ -1,19 +1,7 @@
 from pydantic import BaseModel
 
 
-class EmotionRecognitionResponseImage(BaseModel):
+class EmotionRecognitionResponse(BaseModel):
     bbox: list[int]
     emotion: str
-
-
-class EmotionRecognitionResponse(EmotionRecognitionResponseImage):
     track_id: int
-
-
-class EmotionRecognitionResponseVideo(BaseModel):
-    task_id: str
-    emotion_proportion: dict[str, float]
-
-
-class EmotionRecognitionRequestData(BaseModel):
-    task_id: str
